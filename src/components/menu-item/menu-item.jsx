d3b30d0ad56a6d897baae7608b49acddacd78522
Akndju <davidd.akindoju@gmail.com>
2020-10-09 17:40:17 +0100

    added styles to files on homepage

---
+++ src/components/menu-item/menu-item.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './menu-item.scss';
+
+const MenuItem = ({ title, imageUrl }) => (
+  <div
+    style={{
+      backgroundImage: `url(${imageUrl})`,
+    }}
+    className={(`{size}`, 'menu-item')}
+  >
+    <div className="content">
+      <h1 className="title">{title}</h1>
+      <span className="subtitle">Shop Now</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
