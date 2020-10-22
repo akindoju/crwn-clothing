98d50e6d57e8caaeb43d2e0350d34dc12e1864b4
Akndju <davidd.akindoju@gmail.com>
2020-10-22 14:32:53 +0100

    updated shp data with new data, added cart utils to group items in cart reducer

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -4,12 +4,12 @@ import CollectionItem from '../collection-item/collection-item';
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
+    <h1 className="title">{title}</h1>
     <div className="preview">
       {items
         .filter((item, i) => i < 4)
-        .map(({ id, ...otherItemProps }) => (
-          <CollectionItem key={id} {...otherItemProps} />
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
